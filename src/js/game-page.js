6173eab81c3cd5e971e343144f0c6853e7568438
kung036 <sinariari3@gmail.com>
2024-05-07 01:52:41 +0900

    sytle. 게임 페이지 디자인 수정

--- src/js/game-page.js
+++ src/js/game-page.js
@@ -1,6 +1,5 @@
 export function changeGamePage(url) {
   const game_id = url.split("#/game/")[1];
-  alert(game_id);
   window.localStorage.setItem("game_id", game_id);
   window.location.hash = "/game";
   window.location.reload();
