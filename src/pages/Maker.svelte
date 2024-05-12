159d7caf5580866a4f661c2c0c3697f09c2c9251
kung036 <sinariari3@gmail.com>
2024-05-12 22:46:07 +0900

    fix. 게임 만들기 게임 수정

--- src/pages/Maker.svelte
+++ src/pages/Maker.svelte
@@ -30,14 +30,14 @@
     // 단어들이 알파벳이거나 같은 단어가 있으면 생성 안됨
     for (const word in form.words) {
       console.log("word : ", word);
-      if (!/^[a-zA-Z]+$/.test(word)) {
-        alert("영어만 입력 가능합니다");
-        return;
-      }
-      if (form.words.includes(word)) {
-        alert("서로 다른 단어를 입력하세요");
-        return;
-      }
+      // if (!/^[a-zA-Z]+$/.test(word)) {
+      //   alert("영어만 입력 가능합니다");
+      //   return;
+      // }
+      // if (form.words.includes(word)) {
+      //   alert("서로 다른 단어를 입력하세요");
+      //   return;
+      // }
     }
 
     // word 대문자로 변경
